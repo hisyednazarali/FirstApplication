601bd1701fe69e26517a725fa18414e95ada1af2
hisyednazarali <syednazarali.in@gmail.com>
2022-06-09 23:13:33 +0530

    Angular App

---
+++ src/app/home/QuickSearch.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector :'app-quick-search',
+    templateUrl:'./QuickSearch.component.html',
+    styleUrls:['./QuickSearch.component.css']
+})
+export class QuickSearchComponent implements OnInit{
+
+
+
+
+
+    ngOnInit(): void {
+       
+    }
+    
+}
